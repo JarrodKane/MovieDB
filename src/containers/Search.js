992f5d71057034d1febe1a9b15158242f55a34c4
TrojinCat <kajarrod@gmail.com>
2019-07-04 23:24:29 +1000

    OAuth first run complete and working

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -13,47 +13,107 @@ class Search extends React.Component {
         { id: "243", title: "dingo", add: false }
       ],
       isSignedIn: false,
-      rqtoken: { success: false, expires_at: "", request_token: "" }
+      rqtoken: {},
+      login: { un: "Harrod", pw: "15Mackdog$" }
     };
 
     this.createRequestToken = this.createRequestToken.bind(this);
-    this.createSession = this.createSession.bind(this);
-    this.createSessionWithLogin = this.createSessionWithLogin.bind(this);
-    this.deleteSession = this.deleteSession.bind(this);
+    this.authReq = this.authReq.bind(this);
   }
 
+  // Static prop set for API since it will never be changed by any event in the application
   static defaultProps = {
     api: "9ccbc3e0393b7578cbf2eb8ae9f260c0"
   };
 
-  createRequestToken() {
+  // This function will go create a request_token, which then gets stored in state.
+  async createRequestToken() {
     const api = this.props.api;
-    axois
-      .get(`authentication/token/new?api_key=${api}`)
-      .then(response => console.log(response.data))
-      .catch(function(err) {
-        console.log(err);
-      });
+    try {
+      let rtData = {};
+
+      let res = await axois.get(`authentication/token/new?api_key=${api}`);
+      rtData = res.data;
+      this.setState({ rqtoken: rtData });
+      // console.log(this.state.rqtoken);
+      this.authReq();
+    } catch (e) {
+      alert(e);
+      this.setState({ loading: false });
+    }
   }
 
-  createSession() {
+  // This function will take the created request_token and authenticate it against a user
+  async authReq() {
     const api = this.props.api;
+    const { un, pw } = this.state.login;
+    const rt = this.state.rqtoken.request_token;
+    //  console.log(rt);
 
-    axois.get(`authentication/session/new?api_key=${api}`).then(
-      response => response.data,
-      (err, result) => {
-        console.log(result);
-      }
-    );
+    var data = {
+      username: un,
+      password: pw,
+      request_token: rt
+    };
+
+    try {
+      let authData = {};
+
+      let res = await axois.post(
+        `authentication/token/validate_with_login?api_key=${api}`,
+        data
+      );
+      authData = res.data;
+      console.log("Author data ");
+      console.log(authData);
+      this.createSessionId();
+    } catch (e) {
+      alert(e);
+      this.setState({ loading: false });
+    }
   }
 
-  createSessionWithLogin(userName, password) {
-    // const rt = this.state.token.request_token;
+  //Taking the autenticated request_token we can now get a session ID for the user that has that token
+  async createSessionId() {
+    const api = this.props.api;
+    const rt = this.state.rqtoken.request_token;
+    console.log("request_token to be autneticated for sessionID");
+    console.log(rt);
+    var data = {
+      request_token: rt
+    };
+    try {
+      //let sessionData = {};
+
+      let res = await axois.post(
+        `authentication/session/new?api_key=${api}`,
+        data
+      );
+      console.log("SessionDATA");
+      console.log(res.data);
+      //sessionData = this.state.login;
+
+      // this.setState({ login: sessionData });
+      // console.log(this.state.rqtoken);
+    } catch (e) {
+      alert(e);
+      this.setState({ loading: false });
+    }
   }
+
+  /*
+  subUserLogin() {
+    //TODO: Grab username and pw from form
+  }
+
+  testUser = () => {
+    console.log(this.state.login);
+  };
 
   deleteSession() {}
 
   addOrRemove(id) {}
+  */
 
   render() {
     const rows = this.state.movies.map(row => (
@@ -66,7 +126,9 @@ class Search extends React.Component {
     ));
     return (
       <div>
+        <button onClick={this.testUser}>test user</button>
         <button onClick={this.createRequestToken}>Click</button>
+        <button onClick={this.authReq}>Submit Login</button>
         <SearchBar />
         <div>{rows}</div>
       </div>
