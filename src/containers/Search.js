e1537a0030a910d89fefed4cbff0ecf40ed2891a
TrojinCat <kajarrod@gmail.com>
2019-07-04 18:04:55 +1000

    Added basic api calls

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axois from "../api/TheMovieDB";
 
 import MovieRow from "../components/MovieRow";
 import SearchBar from "../components/SearchBar";
@@ -11,9 +12,46 @@ class Search extends React.Component {
         { id: "213", title: "Hacker", add: false },
         { id: "243", title: "dingo", add: false }
       ],
-      isSignedIn: false
+      isSignedIn: false,
+      rqtoken: { success: false, expires_at: "", request_token: "" }
     };
+
+    this.createRequestToken = this.createRequestToken.bind(this);
+    this.createSession = this.createSession.bind(this);
+    this.createSessionWithLogin = this.createSessionWithLogin.bind(this);
+    this.deleteSession = this.deleteSession.bind(this);
   }
+
+  static defaultProps = {
+    api: "9ccbc3e0393b7578cbf2eb8ae9f260c0"
+  };
+
+  createRequestToken() {
+    const api = this.props.api;
+    axois
+      .get(`authentication/token/new?api_key=${api}`)
+      .then(response => console.log(response.data))
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
+
+  createSession() {
+    const api = this.props.api;
+
+    axois.get(`authentication/session/new?api_key=${api}`).then(
+      response => response.data,
+      (err, result) => {
+        console.log(result);
+      }
+    );
+  }
+
+  createSessionWithLogin(userName, password) {
+    // const rt = this.state.token.request_token;
+  }
+
+  deleteSession() {}
 
   addOrRemove(id) {}
 
@@ -28,6 +66,7 @@ class Search extends React.Component {
     ));
     return (
       <div>
+        <button onClick={this.createRequestToken}>Click</button>
         <SearchBar />
         <div>{rows}</div>
       </div>
